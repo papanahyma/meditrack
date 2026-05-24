d9dd9a6d918e58a3503d3c823156b9d82070c187
papanahyma <papanahyma@gmail.com>
2026-05-24 15:25:31 +0530

    updated frontend

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import dotenv from 'dotenv'
 dotenv.config()
 
+import cors from 'cors';
 import express from 'express'
 import connectDB from './config/db.js'
 import authRoutes from './routes/authRoutes.js'
@@ -32,17 +33,29 @@ webpush.setVapidDetails(
   process.env.VAPID_PRIVATE_KEY
 )
 
-// MODIFIED: Deleted duplicate webpush.setVapidDetails
 
 // CORS
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:5173')
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization')
-  res.header('Access-Control-Allow-Credentials', 'true')
-  if (req.method === 'OPTIONS') return res.sendStatus(200)
-  next()
-})
+const allowedOrigins = [
+    'http://localhost:5173',
+    'http://localhost:3000',
+    process.env.FRONTEND_URL
+].filter(Boolean);
+
+app.use(cors({
+    origin: function (origin, callback) {
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.includes(origin)) {
+            return callback(null, true);
+        } else {
+            console.log('Blocked by CORS:', origin);
+            return callback(new Error('Not allowed by CORS'));
+        }
+    },
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
